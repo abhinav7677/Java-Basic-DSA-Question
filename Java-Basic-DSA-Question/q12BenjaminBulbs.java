6f9aba127a832f64b7c8a35cca303e590ee5bc16
Abhinav <125545001+abhinav7677@users.noreply.github.com>
2023-09-10 12:13:11 +0530

    Update q12BenjaminBulbs.java

--- Java-Basic-DSA-Question/q12BenjaminBulbs.java
+++ Java-Basic-DSA-Question/q12BenjaminBulbs.java
@@ -3,8 +3,6 @@ public class q12BenjaminBulbs {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
 
-        //Question was a riddle
-        //We need to print all the perfect squares between 1 and end range by user.
 
         int n=sc.nextInt();
         for(int i=1; i*i<n;i++){
